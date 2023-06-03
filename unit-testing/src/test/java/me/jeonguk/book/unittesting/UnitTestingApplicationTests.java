8b5623f34a133535957bbd8a1fde5c72c375e3c9
JeongUk Park <dnr1105@gmail.com>
2023-06-03 17:13:38 +0900

    feat: Add lab code

---
+++ unit-testing/src/test/java/me/jeonguk/book/unittesting/UnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jeonguk.book.unittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
